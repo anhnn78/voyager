7aeed2eb4e6c4ed551a1168a37ecd8ff9f75c3a7
Ngô Anh <anhnn@vccloud.vn>
2020-01-13 10:17:25 +0700

    Remove old tinymce setup

--- resources/assets/js/voyager_tinymce.js
+++ resources/assets/js/voyager_tinymce.js
@@ -17,35 +17,6 @@ $(document).ready(function(){
       }
   });
 
-  tinymce.init({
-    menubar: false,
-    selector:'textarea.richTextBox',
-    skin_url: $('meta[name="assets-path"]').attr('content')+'?path=js/skins/voyager',
-    min_height: 600,
-    resize: 'vertical',
-    plugins: 'link, image, code, table, textcolor, lists',
-    extended_valid_elements : 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
-    file_browser_callback: function(field_name, url, type, win) {
-            if(type =='image'){
-              $('#upload_file').trigger('click');
-            }
-        },
-    toolbar: 'styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image table | code',
-    convert_urls: false,
-    image_caption: true,
-    image_title: true,
-    init_instance_callback: function (editor) {
-        if (typeof tinymce_init_callback !== "undefined") {
-            tinymce_init_callback(editor);
-        }
-    },
-    setup: function (editor) {
-        if (typeof tinymce_setup_callback !== "undefined") {
-            tinymce_setup_callback(editor);
-        }
-    }
-  });
-
     tinymce.init({
         selector:'textarea.richTextBox',
         plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
