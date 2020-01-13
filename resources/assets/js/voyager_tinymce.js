f6ecd65a3ccef35e707247c914de9eb3f93f0ce5
Ngô Anh <anhnn@vccloud.vn>
2020-01-13 09:56:51 +0700

    Update tinymce

--- resources/assets/js/voyager_tinymce.js
+++ resources/assets/js/voyager_tinymce.js
@@ -46,4 +46,62 @@ $(document).ready(function(){
     }
   });
 
+    tinymce.init({
+        selector:'textarea.richTextBox',
+        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+        imagetools_cors_hosts: ['picsum.photos'],
+        menubar: 'file edit view insert format tools table help',
+        toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+        toolbar_sticky: true,
+        resize: 'vertical',
+        autosave_ask_before_unload: true,
+        autosave_interval: "30s",
+        autosave_prefix: "{path}{query}-{id}-",
+        autosave_restore_when_empty: false,
+        autosave_retention: "2m",
+        extended_valid_elements : 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
+        image_advtab: true,
+        content_css: [
+            '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+            '//www.tiny.cloud/css/codepen.min.css'
+        ],
+        link_list: [
+            { title: 'My page 1', value: 'http://www.tinymce.com' },
+            { title: 'My page 2', value: 'http://www.moxiecode.com' }
+        ],
+        image_list: [
+            { title: 'My page 1', value: 'http://www.tinymce.com' },
+            { title: 'My page 2', value: 'http://www.moxiecode.com' }
+        ],
+        image_class_list: [
+            { title: 'None', value: '' },
+            { title: 'Some class', value: 'class-name' }
+        ],
+        importcss_append: true,
+        height: 400,
+        file_browser_callback: function(field_name, url, type, win) {
+            if(type =='image'){
+                $('#upload_file').trigger('click');
+            }
+        },
+        init_instance_callback: function (editor) {
+            if (typeof tinymce_init_callback !== "undefined") {
+                tinymce_init_callback(editor);
+            }
+        },
+        setup: function (editor) {
+            if (typeof tinymce_setup_callback !== "undefined") {
+                tinymce_setup_callback(editor);
+            }
+        },
+        min_height: 600,
+        convert_urls: false,
+        image_caption: true,
+        image_title: true,
+        quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+        noneditable_noneditable_class: "mceNonEditable",
+        toolbar_drawer: 'sliding',
+        contextmenu: "link image imagetools table",
+    });
+
 });
